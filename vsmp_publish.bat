cf117760b19a0f40905e7bc9bcaaa6b68ee666ae
Greg Trevellick <gtrevellick@googlemail.com>
2021-12-10 20:56:27 +0000

    ..

--- vsmp_publish.bat
+++ vsmp_publish.bat
@@ -1,6 +1,6 @@
 echo off
 
-CD C:\Program Files\Microsoft Visual Studio\2022\Preview\VSSDK\VisualStudioIntegration\Tools\Bin
+CD D:\Program Files\Microsoft Visual Studio\2022\Community\VSSDK\VisualStudioIntegration\Tools\Bin
 
 VsixPublisher.exe publish -payload "D:\_git\OpenInApp\Src\OpenInApp\bin\Debug\OpenInApp.vsix" -publishManifest "D:\_git\OpenInApp\Src\VsmpPublish.json" -personalAccessToken "vsmp_pat"
 
