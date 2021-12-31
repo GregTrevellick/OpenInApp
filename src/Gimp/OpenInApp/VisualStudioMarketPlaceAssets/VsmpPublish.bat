7135e7ae2b968bb2415c74f8587fac5bbaadd86a
Greg Trevellick <gtrevellick@googlemail.com>
2021-12-31 07:15:12 +0000

    launcher

--- src/Gimp/OpenInApp/VisualStudioMarketPlaceAssets/VsmpPublish.bat
+++ src/Gimp/OpenInApp/VisualStudioMarketPlaceAssets/VsmpPublish.bat
@@ -2,6 +2,6 @@ echo off
 
 CD D:\Program Files\Microsoft Visual Studio\2022\Community\VSSDK\VisualStudioIntegration\Tools\Bin
 
-VsixPublisher.exe publish -payload "D:\_git\OpenInApp\Src\PaintDotNet\OpenInApp\bin\Debug\OpenInPaintDotNet.vsix" -publishManifest "D:\_git\OpenInApp\Src\PaintDotNet\VsmpPublish.json" -personalAccessToken "vsmp_pat"
+VsixPublisher.exe publish -payload "D:\_git\OpenInApp\Src\Gimp\OpenInApp\bin\Debug\OpenInGimp.vsix" -publishManifest "D:\_git\OpenInApp\Src\Gimp\OpenInApp\VisualStudioMarketPlaceAssets\VsmpPublish.json" -personalAccessToken "vsmp_pat"
 
 REM https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2019
--- src/MarkdownMonster/VsmpPublish.bat
+++
@@ -1,7 +0,0 @@
-echo off
-
-CD D:\Program Files\Microsoft Visual Studio\2022\Community\VSSDK\VisualStudioIntegration\Tools\Bin
-
-VsixPublisher.exe publish -payload "D:\_git\OpenInApp\Src\MarkdownMonster\OpenInApp\bin\Debug\OpenInMarkdownMonster.vsix" -publishManifest "D:\_git\OpenInApp\Src\MarkdownMonster\VsmpPublish.json" -personalAccessToken "vsmp_pat"
-
-REM https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2019
